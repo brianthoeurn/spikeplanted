c059e9c9e66065bf4db73266c6ec02aba63cfb29
brianthoeurn <brianmonirath@hotmail.com>
2022-07-20 14:59:15 -0700

    Agents

---
+++ src/components/AgentList/AgentList.js
@@ -0,0 +1,30 @@
+import "./AgentList.css";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+function AgentList() {
+  const [allAgents, setallAgents] = useState([]);
+  const loadAgent = () => {
+    fetch("https://valorant-api.com/v1/agents/")
+      .then((res) => res.json())
+      .then((data) => setallAgents(data.data));
+  };
+
+  useEffect(() => {
+    loadAgent();
+  }, []);
+
+  let ShowAgentList = allAgents.map((item) => {
+    if (item.isPlayableCharacter === true)
+      return (
+        <div key={item.fullPortrait}>
+          <Link to={"/agents" + item.fullPortrait}>
+            <img className="allAgents" src={item.fullPortrait} alt="test"></img>
+          </Link>
+        </div>
+      );
+  });
+  return <div>{ShowAgentList}</div>;
+}
+
+export default AgentList;
