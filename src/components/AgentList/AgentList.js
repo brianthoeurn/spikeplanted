3775afd843b35323705357a27ec39cb1f1d59706
brianthoeurn <brianmonirath@hotmail.com>
2022-07-22 01:25:52 -0700

    testing

--- src/components/AgentList/AgentList.js
+++ src/components/AgentList/AgentList.js
@@ -8,26 +8,54 @@ function AgentList() {
   const loadAgent = () => {
     fetch("https://valorant-api.com/v1/agents/")
       .then((res) => res.json())
-      .then((data) => setallAgents(data.data));
+      .then((data) => {
+        const customData = data.data;
+        console.log(data.data);
+        customData.push({
+          uuid: "https://www.linkedin.com/in/brianmonirath",
+          displayName: "Monirath",
+          fullPortrait:
+            "https://cdn.discordapp.com/attachments/853891866575896586/984172384191909908/unknown.png",
+          isPlayableCharacter: true,
+          isCustomCharacter: true,
+        });
+        setallAgents(customData);
+      });
   };
 
   useEffect(() => {
     loadAgent();
   }, []);
 
-  const ShowAgentList = allAgents.map((picture) => {
-    if (picture.isPlayableCharacter === true)
-      return (
-        <div key={picture.fullPortrait}>
-          
-          <Link to={"/agents/" + picture.uuid}>
-            <p>{picture.displayName}</p>
-              <img className="Agents" src={picture.fullPortrait} alt="test"/>
-          </Link>
-        </div>
-      ); 
+  const ShowAgentList = allAgents.map((data) => {
+    if (data.isPlayableCharacter === true) {
+      if (data.isCustomCharacter === true) {
+        return (
+          <div key={data.fullPortrait} className="agentCard">
+            <a href={data.uuid} target="_blank" rel="noreferrer">
+              <p className="agentName">{data.displayName}</p>
+              <img className="Agents" src={data.fullPortrait} alt="test" />
+            </a>
+          </div>
+        );
+      } else {
+        return (
+          <div key={data.fullPortrait} className="agentCard">
+            <Link to={"/agents/" + data.uuid}>
+              <p className="agentName">{data.displayName}</p>
+              <img className="Agents" src={data.fullPortrait} alt="test" />
+            </Link>
+          </div>
+        );
+      }
+    }
+    return <></>;
   });
-  return <div className="allAgents">{ShowAgentList}</div>;
+  return (
+    <div className="agentListBody">
+      <div className="allAgents">{ShowAgentList}</div>
+    </div>
+  );
 }
 
 export default AgentList;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,32 @@
 import "./Home.css";
-import bgFootage from './bgFootage.mp4'
+import gamePlay from "./123.mp4";
 
 function Home() {
   return (
-    <div>
-      <header className="header">Welcome to Spike Planted</header>
-      
-      <video src={bgFootage} autoPlay loop muted />
-      <p>
-        Valorant is a 5v5 tactical shooter game where players will be able to play as an agent with special abilties! Here you will find the general information of
-        each agent's ability, roles they specialize in, and many more!
+    <div className="why">
+      <p className="valInfo">
+        Valorant is a 5v5 tactical shooter game where players will be able to
+        play as an agent with special abilties! Here you will find the general
+        information of each agent's ability, roles they specialize in, and many
+        more!
       </p>
+      <div className="bgVideo">
+        <video
+          width={550}
+          src={gamePlay}
+          autoPlay
+          loop
+          muted
+        />
+      </div>
+      <a
+        href="https://playvalorant.com/en-us/"
+        target="_blank"
+        rel="noreferrer"
+        className="playNow"
+      >
+        Play Now
+      </a>
     </div>
   );
 }
