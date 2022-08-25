730533664824ab96307acd9f214c0c745e56f5d1
brianthoeurn <brianmonirath@hotmail.com>
2022-08-24 18:35:56 -0700

    reboot

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home/Home";
 import AgentList from "./components/AgentList/AgentList";
-import Arsenal from "./components/Arsenal/Arsenal";
+// import Arsenal from "./components/Arsenal/Arsenal";
 import ThisAgent from './components/Agents/Agents'
 import "./App.css";
 
 function App() {
   return (
     <div>
-      
       <div className="ValCover">
-      <p className="header">Welcome to Spike Planted</p>
-           <nav className="links">
-              <Link to="/"> <h1>Home</h1> </Link>
-              <Link to="/agentlist"> <h1>Agents</h1> </Link>
-              <Link to="/arsenal">  <h1>Arsenal</h1>  </Link>
-           </nav>
+        <p className="header">Welcome to Spike Planted</p>
+        <nav className="links">
+          <Link to="/"> <h1>Home</h1> </Link>
+          <Link to="/agentlist"> <h1>Agents</h1> </Link>
+          {/* <Link to="/arsenal">  <h1>Arsenal</h1>  </Link> */}
+          </nav>
       </div>
       <main>
         <Routes>
           <Route path="" element={<Home />} />
           <Route path="/agentlist" element={<AgentList />}/>
-          <Route path="/arsenal" element={<Arsenal />}/>
+          {/* <Route path="/arsenal" element={<Arsenal />}/> */}
           <Route path='/agents/:uuid' element={<ThisAgent />}/>
-          <Route path='/arsenal/:displayName' element={<ThisAgent />}/>
+          {/* <Route path='/arsenal/:displayName' element={<ThisAgent />}/> */}
         </Routes>
       </main> 
     </div> 
